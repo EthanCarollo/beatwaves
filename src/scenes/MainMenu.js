19ba87d2db9f59113115f0df518ed98eeed4406e
TsuKiZo <53433445+xTsuKiZox@users.noreply.github.com>
2023-11-21 15:23:11 +0100

    feat(mainmenu): add area for hand navigation

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,32 +1,63 @@
 /**
  * This is the Main Menu scene, played as the end of each game
  */
-function MainMenu()
-{
-    var slider 
+function MainMenu() {
+    let slider
+
+    let poseNet;
+    let poses;
+    let sceneIsLoaded = false;
+    let lifeTime = 60;
+    let handPosition = {
+        x: 0,
+        y: 0
+    }
+
+    // This is the options for load pose net
+    let poseNetOptions = {
+        imageScaleFactor: 0.3,
+        minConfidence: 0.5,
+        maxPoseDetections: 1,
+        flipHorizontal: true,
+        outputStride: 16,
+        multiplier: 0.75,
+        inputResolution: 321,
+        nmsRadius: 30
+    }
+
     // enter() will be executed each time the SceneManager switches
     // to this Scene
-    this.enter = function()
-    {
-        background(255,255,255)
-        this.setCarousselSlide() 
+    this.enter = function () {
+
+        initializeCenterOfWindow()
+        frameRate(60)
+
+        poseNet = ml5.poseNet(video, poseNetOptions, this.modelLoaded);
+        poseNet.on('pose', (results) => { poses = results; }); // Just set the poses var on the event pose
+
+
+        background(255, 255, 255)
+        this.setCarousselSlide()
         // Create and mount the slider with splide, show the documentation here : https://splidejs.com/guides/
         this.slider = new Splide('#splide', {
             type: 'loop',
             perPage: 3,
             focus: 'center',
-            arrows:false,
-            autoplay: true,
+            arrows: false,
+            autoplay: false,
             interval: 8000,
             flickMaxPages: 1,
             updateOnMove: true,
             pagination: false,
             padding: '-5%',
             gap: '3vw'
-          }).mount();
+        }).mount();
+
+        this.slider.go("+1");
+
     }
 
-    this.setCarousselSlide = function(){
+    this.setCarousselSlide = function () {
         // Show the game caroussel
         document.getElementById("game_caroussel").style.display = "flex"
         let splideList = document.getElementById("splide__list")
@@ -36,9 +67,9 @@ function MainMenu()
             // Appends the text document
             let doc = document.createElement("div")
             doc.classList.add("song_element")
-            doc.style.backgroundImage = "url("+Assets.get("IMAGES").data[0].url+")";
+            doc.style.backgroundImage = "url(" + Assets.get("IMAGES").data[0].url + ")";
             doc.classList.add("splide__slide")
-            doc.innerHTML = "<h1>"+song.nameSong+"</h1>"
+            doc.innerHTML = "<h1>" + song.nameSong + "</h1>"
 
             let song_button = document.createElement("button")
             song_button.innerHTML = "Choose Music"
@@ -46,7 +77,7 @@ function MainMenu()
                 console.log(song.nameSong)
                 initializeMusic(song)
                 this.goNextScene()
-            })   
+            })
             doc.appendChild(song_button)
             splideList.appendChild(doc)
 
@@ -54,12 +85,104 @@ function MainMenu()
     }
 
     // draw() is the normal draw function, this function work like a scene
-    this.draw = function(){
+    this.draw = function () {
+        if (sceneIsLoaded === false) return;
 
+        if (DEBUGMODE === true) {
+            this.debugScene();
+        }
+
+        this.registerHandPosition()
+        this.navigateMenu()
+    }
+
+    this.registerHandPosition = () => {
+        if (!poses || !poses[0])
+            return
+
+        var pose = poses[0];
+
+        handPosition = pose.pose.rightWrist
+        //if (_rightWrist.confidence > 0.6) handPoseHistory.right.push(_rightWrist);
+    }
+
+    this.getHandForHistory = (hand) => {
+        return {
+            position: createVector(hand.x, hand.y),
+            confidence: hand.confidence,
+            life: lifeTime
+        }
+    }
+
+    // Function called once model is loaded
+    this.modelLoaded = () => {
+        sceneIsLoaded = true
+        if (DEBUGMODE === true)
+            console.log('/-Model Loaded, you can play-/');
     }
 
     this.goNextScene = () => {
         document.getElementById("game_caroussel").style.display = "none"
         SceneManager.showNextScene()
+    }
+
+    // Function called if DEBUGMODE const is true
+    this.debugScene = () => {
+
+        // Flip video horizontaly
+        scale(-1, 1);
+        image(video, -width, 0, width, height)
+        scale(-1, 1);
+
+        if (poses) {
+            this.drawDebugPose(poses[0])
+        }
+    }
+
+    // Function called when we need to show the pose
+    this.drawDebugPose = (pose) => {
+        if (!pose)
+            return
+
+        // Every position to draw on sketch
+        const positionArray = [
+            pose.pose.rightShoulder,
+            pose.pose.rightElbow,
+            pose.pose.rightWrist,
+            pose.pose.rightEye,
+            pose.pose.rightAnkle,
+        ]
+
+        // For the debug, i boucle on the array of position and then draw every parts of the body
+        // And for the color, i just check if we here in the second half of the array or not
+        // Fill the color of the circle with the confidence
+        for (let i = 0; i < positionArray.length; i++) {
+            const elementPosition = positionArray[i];
+            (i > (positionArray.length - 1) / 2) ? fill(255, 0, 0, elementPosition.confidence * 255) : fill(0, 255, 0, elementPosition.confidence * 255)
+            circle(elementPosition.x, elementPosition.y, 30)
+        }
+
+    }
+
+    this.navigateMenu = () => {
+        if (DEBUGMODE) {
+            // console.log("==== " + handPosition.x + "X ====== ")
+            // console.log("==== " + handPosition.y + "Y ====== ")
+        }
+        let margin = 140
+        let widthMenu = width - margin - 200
+        let heightMenu = height / 2 - 200 / 2
+        let waitingHand = 0
+
+
+        if (isInRect(handPosition.x, handPosition.y, widthMenu, heightMenu, 300)) {
+            console.log("Je suis a droite")
+        } 
+        else if (isInRect(handPosition.x, handPosition.y, width / 2 - 200 / 2, heightMenu + margin, 200)) {
+            console.log("Je suis au milieu")
+        } 
+        else if (isInRect(handPosition.x, handPosition.y, margin, heightMenu, 300)) {
+            console.log("Je suis a gauche")
+        }
     }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,18 @@
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomBool() {
+  return getRandomInt(2) === 1
+}
+
+
+
+function isInRect(pointX, pointY, squareX, squareY, squareSize) {
+  if (DEBUGMODE) {
+    fill(255, 0, 0, 80)
+    rect(squareX, squareY, squareSize)
   }
 
-function getRandomBool(){
-    return getRandomInt(2) === 1
+  return (pointX >= squareX && pointX <= squareX + squareSize && pointY >= squareY && pointY <= squareY + squareSize)
 }
