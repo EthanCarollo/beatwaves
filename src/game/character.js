bb77a94873e5f69dc787511a4465a30570f4dfed
TsuKiZo <53433445+xTsuKiZox@users.noreply.github.com>
2023-11-23 13:54:38 +0100

    feat(character): start of adding in-game images

--- src/game/character.js
+++ src/game/character.js
@@ -1,8 +1,41 @@
-function character(){
-
+function character(status) {
+    
     let characterDiv = document.getElementById("character")
 
+    let imgCharacter = {
+        "mainMenu": Assets.get("IMAGES").data[2].url,
+        "combo": Assets.get("IMAGES").data[3].url,
+        "ratingAbove": Assets.get("IMAGES").data[4].url,
+        "ratingF": Assets.get("IMAGES").data[5].url,
+    }
 
-    console.log(Assets.get("IMAGES").data[1])
 
+
+    switch (status) {
+        case "mainMenu":
+            characterDiv.style.display = "block"
+            characterDiv.src = imgCharacter.mainMenu
+            break;
+
+        case "ratingF":
+            characterDiv.style.display = "block"
+            characterDiv.src = imgCharacter.ratingF
+            break;
+
+        case "ratingAbove":
+            characterDiv.style.display = "block"
+            characterDiv.src = imgCharacter.aboveB
+            break;
+
+        case "combo":
+            characterDiv.style.display = "block"
+            characterDiv.src = imgCharacter.combo
+            break;
+
+        default:
+            break;
+    }
+
+    //    console.log(Assets.get("IMAGES"))
+    // console.log(Assets.get("IMAGES").data[1])
 }
