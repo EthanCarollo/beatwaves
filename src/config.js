7dc77100c7b1c23fe721ffabbcb66535df16c288
JackyTuneur <deku.private@gmail.com>
2023-11-24 10:16:21 +0100

    refactor(body): set one instance of body in intro

--- src/config.js
+++ src/config.js
@@ -6,9 +6,24 @@ const ASSETSPATH = {
     INSTRUMENTS: "./resources/instruments/instruments.json",
     IMAGES: "./resources/image.json"
 }
-
 var isAssetsLoaded = false;
+
 var sceneIsLoad = false;
 var audioIsLoad = false;
 
+
+
+// Pose net var
 var video = undefined;
+var poseNet;
+var poses;
+var poseNetOptions = {
+    imageScaleFactor: 0.3,
+    minConfidence: 0.5,
+    maxPoseDetections: 1,
+    flipHorizontal: true,
+    outputStride: 16,
+    multiplier: 0.75,
+    inputResolution: 257,
+    nmsRadius: 30
+}
