84914a1c62eb1bb125b49f5dd21c3e9980df9b45
JackyTuneur <deku.private@gmail.com>
2023-12-04 10:34:44 +0100

    up: add style to card menu

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -4,7 +4,6 @@ let mouseSize = 80;
 const showInteractiveButton = (interactiveButton, checkPosition) => {
 
     if (isInRect(checkPosition.x, checkPosition.y, interactiveButton.position.x, interactiveButton.position.y, interactiveButton.width, interactiveButton.height) && interactiveButton.isReady) {
-        console.log("Is lerping in ")
         interactiveButton.loading = lerp(interactiveButton.loading, 100, 0.1)
         if(interactiveButton.loading > 99.9){
             interactiveButton.callback()
